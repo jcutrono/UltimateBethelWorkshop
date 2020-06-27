5522ef3ed17865604878310a07eb90a4c9d5767f
Joseph Cutrono <jcutrono@gmail.com>
2020-06-27 16:47:47 -0400

    add test for valid state

--- CovidTest/Covid.UnitTests/LocationTest.cs
+++ CovidTest/Covid.UnitTests/LocationTest.cs
@@ -12,14 +12,32 @@ namespace Covid.UnitTests
     public class UnitTest1
     {
         [Fact]
-        public void Test()
+        public void Test_Get_All_Sites()
         {
+            // Arrange
             var data = new Mock<ILocationData>();
             data.Setup(x => x.Get()).Returns(new List<Location>{ new Location { Name = "Test"}});
             var locationService = new LocationService(data.Object);
-
+            
+            // Act
             var locations = locationService.Get().ToList();
 
+            // Assert
+            Assert.Equal("Test", locations[0].Name);
+        }
+
+        [Fact]
+        public void Test_Get_All_Sites_For_A_State()
+        {
+            // Arrange
+            var data = new Mock<ILocationData>();
+            data.Setup(x => x.Get(It.IsAny<string>())).Returns(new List<Location>{ new Location { Name = "Test"}});
+            var locationService = new LocationService(data.Object);
+            
+            // Act
+            var locations = locationService.Get("AA").ToList();
+
+            // Assert
             Assert.Equal("Test", locations[0].Name);
         }
 
